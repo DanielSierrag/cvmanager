5c557bec99e0f16d8a5e377fa8602ebd04341d13
Daniel Sierra <danielgreco2209mail.com>
2025-03-14 18:11:34 -0500

    Migrando a mysql

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.13.2-alpine3.21
 
 RUN addgroup app && adduser -S -G app app
+RUN apk add --no-cache \
+    mariadb-dev \
+    gcc \
+    musl-dev \
+    python3-dev
 USER app
 WORKDIR /app/
 COPY --chown=app:app requirements.txt .
--- cvmanager/settings.py
+++ cvmanager/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'cvmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME', 'mysql'),
+        'USER': os.getenv('DATABASE_USER', 'root'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),
+        'PORT': os.getenv('DATABASE_PORT', '3306'),
     }
 }
 
